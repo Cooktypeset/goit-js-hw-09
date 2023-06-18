b68c510d6b5bb552210064996a75ac777002e405
Cooktypeset <122571877+Cooktypeset@users.noreply.github.com>
2023-06-18 08:57:47 +0000

    Deploying to gh-pages from @ Cooktypeset/goit-js-hw-09@baa2d7d597f31fb8fc409c8eabd1e214a1be07d5 🚀

---
+++ 01-color-switcher.c71c9535.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("data-start"),e=document.querySelector("data-stop");t=document.addEventListener("click",handleStartClick),e=document.addEventListener("click",handleStopClick);
+//# sourceMappingURL=01-color-switcher.c71c9535.js.map
