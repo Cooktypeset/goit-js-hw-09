ffaf383e4ce28f7f4ffe359aee2a19e140512451
Cooktypeset <tovpyhalitva@gmail.com>
2023-06-18 14:13:31 +0300

    newsecUpd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,28 @@
-const clickStartBtn = document.querySelector('data-start');
-const clickStopBtn = document.querySelector('data-stop');
- clickStartBtn = document.addEventListener('click', handleStartClick);
-clickStopBtn = document.addEventListener('click', handleStopClick);
+const bodyQwery = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+stopBtn.disabled = true;
+bodyQwery.addEventListener('click', onClick);
+
+let limitId = null;
+
+function onClick(event) {
+  if (event.target === startBtn) {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    limitId = setInterval(() => {
+      bodyQwery.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  } else if (event.target === stopBtn) {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(limitId);
+  }
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
